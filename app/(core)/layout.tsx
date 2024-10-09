add99bc2bf7bc54b86d9de606ddb3f6ca497009f
victormusembi11 <victor.musembi11@gmail.com>
2024-10-09 11:16:24 +0300

    feat: Add RootLayout component for consistent page layout

---
+++ app/(core)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/core-navbar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
