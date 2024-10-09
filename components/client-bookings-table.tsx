43cfdd3b9eab4de56996879ef5886e02ce24f207
victormusembi11 <victor.musembi11@gmail.com>
2024-10-09 03:55:42 +0300

    feat: Add client bookings table component

---
+++ components/client-bookings-table.tsx
@@ -0,0 +1,65 @@
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
+
+interface Booking {
+  id: number;
+  userId: number;
+  bookingDate: string;
+  status: string;
+  description: string;
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+  };
+}
+
+interface BookingTableProps {
+  bookings: Booking[];
+}
+
+const BookingTable: React.FC<BookingTableProps> = ({ bookings }) => {
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Booking Details</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Booking ID</Th>
+            <Th>Booking Date</Th>
+            <Th>Status</Th>
+            <Th>Description</Th>
+            <Th>User Name</Th>
+            <Th>User Email</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {bookings.map((booking) => (
+            <Tr key={booking.id}>
+              <Td>{booking.id}</Td>
+              <Td>{new Date(booking.bookingDate).toLocaleDateString()}</Td>
+              <Td>{booking.status}</Td>
+              <Td>{booking.description}</Td>
+              <Td>
+                {booking.user.firstName} {booking.user.lastName}
+              </Td>
+              <Td>{booking.user.email}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>Booking ID</Th>
+            <Th>Booking Date</Th>
+            <Th>Status</Th>
+            <Th>Description</Th>
+            <Th>User Name</Th>
+            <Th>User Email</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default BookingTable;
