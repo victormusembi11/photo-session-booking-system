a2aa7917e3943642fb6f3aed96579b6e2efbb81f
victormusembi11 <victor.musembi11@gmail.com>
2024-10-09 03:04:15 +0300

    chore: Add Chakra UI provider to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,10 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ChakraProvider>{children}</ChakraProvider>
       </body>
     </html>
   );
