0dabdb66192154a507a04e6c56b4d626b1611c84
victormusembi11 <victor.musembi11@gmail.com>
2024-10-09 05:16:03 +0300

    feat: Add booking details page component and API integration

--- app/(admin)/admin/bookings/[id]/page.tsx
+++ app/(admin)/admin/bookings/[id]/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
 export default function Booking() {
-  return <div>Booking</div>;
+  const { id } = useParams();
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["bookings", id],
+    queryFn: () => fetchBookingById(Array.isArray(id) ? id[0] : id),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error instanceof Error ? error.message : "An error occurred"}</div>;
+
+  const booking = data?.booking;
+
+  return (
+    <div>
+      <h1>Booking Details</h1>
+      <p>Booking ID: {booking?.id}</p>
+      <p>Status: {booking?.status}</p>
+      <p>Description: {booking?.description}</p>
+      <p>Booking Date: {new Date(booking?.bookingDate).toLocaleDateString()}</p>
+
+      <h2>User Details</h2>
+      <p>User ID: {booking?.user?.id}</p>
+      <p>
+        Name: {booking?.user?.firstName} {booking?.user?.lastName}
+      </p>
+      <p>Email: {booking?.user?.email}</p>
+    </div>
+  );
+}
+
+async function fetchBookingById(id: string) {
+  try {
+    const response = await fetch(`/api/bookings/${id}`);
+    if (!response.ok) {
+      throw new Error("Error fetching booking");
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching booking: ", error);
+    throw new Error("Error fetching booking");
+  }
 }
