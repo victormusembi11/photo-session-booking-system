58c63ea2290392ab2ec382deda1968db927ef9a8
victormusembi11 <victor.musembi11@gmail.com>
2024-10-09 03:30:29 +0300

    chore: Add Prisma client configuration for database access

---
+++ lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // Ensure the Prisma client is not recreated during hot reloads in development.
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export default prisma;
